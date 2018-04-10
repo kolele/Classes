946681044dbcd942bb7d456d161995e2fa53fa4c
Slobodan <slobodan.kosanovic@gmail.com>
2018-04-10 20:11:48 +0200

    a

---
+++ src/com/kole/Car.java
@@ -0,0 +1,12 @@
+package com.kole;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    
+}
--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -3,6 +3,8 @@ package com.kole;
 public class Main {
 
     public static void main(String[] args) {
-
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.model = "Carrera";
     }
 }
